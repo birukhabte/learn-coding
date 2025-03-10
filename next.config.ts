a02dc863eec399ee2eb759366dcb42212e928205
biruk habte <biruk.habte-ug@aau.edu.et>
2025-03-10 06:39:47 +0300

    implement image handling

--- next.config.ts
+++ next.config.ts
@@ -12,3 +12,4 @@ export default nextConfig;
 2025-03-01T04:22:11 - add responsive design
 2025-03-24T03:17:40 - implement user registration feature
 2025-03-11T17:52:57 - implement CRUD operations
+2025-03-10T06:39:47 - implement image handling
