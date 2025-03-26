61846c85133ae941fb2ddb15dbb593d0fb27a411
biruk habte <biruk.habte-ug@aau.edu.et>
2025-03-26 04:31:02 +0300

    implement real-time updates

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -7,3 +7,4 @@ const config = {
 export default config;
 2025-03-13T07:52:07 - create dashboard layout
 2025-03-06T19:49:05 - initialize project structure
+2025-03-26T04:31:02 - implement real-time updates
