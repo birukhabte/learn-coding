0acad53a0dc001b986771fd0f16e73ff6fa3c7cc
biruk habte <biruk.habte-ug@aau.edu.et>
2025-05-06 04:15:55 +0300

    prepare project for production

--- particles.tsx
+++ particles.tsx
@@ -3,3 +3,4 @@
 2025-05-22T20:37:36 - refactor components
 2025-05-25T11:35:13 - setup code linting
 2025-05-27T18:34:35 - implement dark mode feature
+2025-05-06T04:15:55 - prepare project for production
