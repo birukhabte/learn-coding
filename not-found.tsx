cb73878ed7cb1a54ee997268268370f69e231a0b
biruk habte <biruk.habte-ug@aau.edu.et>
2025-05-11 13:28:00 +0300

    setup file upload feature

--- not-found.tsx
+++ not-found.tsx
@@ -1,3 +1,4 @@
 2025-05-21T01:51:31 - setup basic folder architecture
 2025-05-11T03:11:44 - create user model schema
 2025-05-14T11:18:52 - add caching mechanism
+2025-05-11T13:28:00 - setup file upload feature
