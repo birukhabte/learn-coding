53e7411f645a34ae4ec003fc4e83cddf00c19e01
biruk habte <biruk.habte-ug@aau.edu.et>
2025-05-11 10:49:48 +0300

    create main entry file

--- spinner.tsx
+++ spinner.tsx
@@ -2,3 +2,4 @@
 2025-05-14T19:06:54 - create pagination system
 2025-05-17T04:20:55 - implement CRUD operations
 2025-05-08T02:30:21 - add notifications system
+2025-05-11T10:49:48 - create main entry file
