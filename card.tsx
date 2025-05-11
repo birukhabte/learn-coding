c765cae83baf738e3d792d41a6f8dab68c3712a3
biruk habte <biruk.habte-ug@aau.edu.et>
2025-05-11 11:33:24 +0300

    finalize project features

--- card.tsx
+++ card.tsx
@@ -3,3 +3,4 @@
 2025-05-27T01:31:30 - implement logout functionality
 2025-05-18T07:08:43 - implement CRUD operations
 2025-05-25T15:53:57 - create pagination system
+2025-05-11T11:33:24 - finalize project features
