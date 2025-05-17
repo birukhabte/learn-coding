6d45f6179bf2ce08a3a6b44b78279f3371b52e14
biruk habte <biruk.habte-ug@aau.edu.et>
2025-05-17 08:49:29 +0300

    prepare project for production

--- button.tsx
+++ button.tsx
@@ -1,3 +1,4 @@
 2025-05-08T06:29:30 - implement user login functionality
 2025-05-21T15:30:50 - create API service layer
 2025-05-11T11:06:59 - add multi-language support
+2025-05-17T08:49:29 - prepare project for production
