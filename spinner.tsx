79391bbede4fd040e2e296241e82112a32d593da
biruk habte <biruk.habte-ug@aau.edu.et>
2025-05-17 11:23:57 +0300

    prepare project for production

--- spinner.tsx
+++ spinner.tsx
@@ -3,3 +3,4 @@
 2025-05-17T04:20:55 - implement CRUD operations
 2025-05-08T02:30:21 - add notifications system
 2025-05-11T10:49:48 - create main entry file
+2025-05-17T11:23:57 - prepare project for production
