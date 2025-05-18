d249a8f3d4fa14f21036ff367d61634bfcf07a26
biruk habte <biruk.habte-ug@aau.edu.et>
2025-05-18 07:08:43 +0300

    implement CRUD operations

--- card.tsx
+++ card.tsx
@@ -1,3 +1,4 @@
 2025-05-14T16:58:04 - setup basic folder architecture
 2025-05-30T08:22:24 - implement user login functionality
 2025-05-27T01:31:30 - implement logout functionality
+2025-05-18T07:08:43 - implement CRUD operations
