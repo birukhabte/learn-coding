9e0705ddc81e6221db33a1f59223234aee075b07
biruk habte <biruk.habte-ug@aau.edu.et>
2025-05-18 14:03:56 +0300

    create dashboard layout

--- label.tsx
+++ label.tsx
@@ -3,3 +3,4 @@
 2025-05-17T10:50:24 - create pagination system
 2025-05-25T23:38:06 - implement admin controls
 2025-05-22T12:21:32 - implement CRUD operations
+2025-05-18T14:03:56 - create dashboard layout
