42172c898e7ff15a919b3c965745547ecd6bd035
biruk habte <biruk.habte-ug@aau.edu.et>
2025-05-25 23:38:06 +0300

    implement admin controls

--- label.tsx
+++ label.tsx
@@ -1,3 +1,4 @@
 2025-05-11T10:55:52 - add responsive design
 2025-05-17T13:04:43 - implement logout functionality
 2025-05-17T10:50:24 - create pagination system
+2025-05-25T23:38:06 - implement admin controls
