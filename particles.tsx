101321648ce9ac9e8feb84266b87c340fc803e21
biruk habte <biruk.habte-ug@aau.edu.et>
2026-02-17 22:32:34 +0300

    optimize API performance

--- particles.tsx
+++ particles.tsx
@@ -7,3 +7,4 @@
 2026-02-17T08:58:18 - optimize database queries
 2026-02-23T16:16:08 - add update profile functionality
 2026-02-26T13:47:59 - implement error UI handling
+2026-02-17T22:32:34 - optimize API performance
